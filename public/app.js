bfd2a8bd690795d4954cd5deee16f959eb33e421
Amir Aiman <amiraiman.jj@gmail.com>
2022-09-22 17:45:11 +0800

    Add toggle to mark all / unmark all

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 const notiContainer = document.querySelector("div.noti");
+let readState = true;
 
 notiContainer.addEventListener("click", function (e) {
     const btn = e.target;
@@ -7,6 +8,9 @@ notiContainer.addEventListener("click", function (e) {
     const notis = this.querySelectorAll(".noti-box");
 
     notis.forEach((noti) => {
-        noti.dataset.read = "true";
+        noti.dataset.read = readState.toString();
     });
+
+    readState = !readState;
+    btn.textContent = readState ? "Mark all as read" : "Unmark all as read";
 });
