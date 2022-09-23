92385a296e26dc177bc304617006f0443861bf6d
Amir Aiman <amiraiman.jj@gmail.com>
2022-09-23 09:44:09 +0800

    Change unread noti to match the current unread

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const notiContainer = document.querySelector("div.noti");
 const notiToggle = notiContainer.querySelector("#notiToggle");
 const notis = notiContainer.querySelectorAll(".noti-box");
+const notiUnread = notiContainer.querySelector("#notiUnread");
 
 let readState = true;
 
@@ -11,4 +12,19 @@ notiToggle.addEventListener("click", function () {
 
     readState = !readState;
     this.textContent = readState ? "Mark all as read" : "Unmark all as read";
+    updateUnread(notis, notiUnread);
 });
+
+// Change count for unread messages
+function updateUnread(notiNodeList, unreadEl) {
+    let unread = 0;
+    notiNodeList.forEach((noti) => {
+        unread += noti.dataset.read === "false" ? 1 : 0;
+    });
+
+    unreadEl.textContent = unread;
+}
+
+window.onload = () => {
+    updateUnread(notis, notiUnread);
+};
