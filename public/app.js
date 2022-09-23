b299356e3e6283ec0c12e34a5aa94be379eed633
Amir Aiman <amiraiman.jj@gmail.com>
2022-09-23 09:37:10 +0800

    Fix js to use new button selector

--- public/app.js
+++ public/app.js
@@ -1,16 +1,14 @@
 const notiContainer = document.querySelector("div.noti");
+const notiToggle = notiContainer.querySelector("#notiToggle");
+const notis = notiContainer.querySelectorAll(".noti-box");
+
 let readState = true;
 
-notiContainer.addEventListener("click", function (e) {
-    const btn = e.target;
-    if (!btn.classList.contains("noti-markread")) return;
-
-    const notis = this.querySelectorAll(".noti-box");
-
+notiToggle.addEventListener("click", function () {
     notis.forEach((noti) => {
         noti.dataset.read = readState.toString();
     });
 
     readState = !readState;
-    btn.textContent = readState ? "Mark all as read" : "Unmark all as read";
+    this.textContent = readState ? "Mark all as read" : "Unmark all as read";
 });
